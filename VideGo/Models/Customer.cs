d3524bcba4330417ce1747ad48449298135e1a2f
bdunams <dabadestboy1@gmail.com>
2017-11-19 21:43:52 -0500

    Added MemberType seed migration and ApplyAnnotationToCustomerName migration.

--- VideGo/Models/Customer.cs
+++ VideGo/Models/Customer.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace VideGo.Models
 {
     public class Customer : DbContext
     {   
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
