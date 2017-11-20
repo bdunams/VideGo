488022daa0a4380d3742f2f96eadbb75c31767e1
bdunams <dabadestboy1@gmail.com>
2017-11-19 19:19:26 -0500

    Add project files.

---
+++ VideGo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VideGo.MvcApplication" Language="C#" %>
---
+++ VideGo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideGo.Startup))]
+namespace VideGo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
