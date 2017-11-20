6d821c916a629b0424403bb16c235d2d218af580
bdunams <dabadestboy1@gmail.com>
2017-11-19 20:55:48 -0500

    Added MembershipType Model and modified Customer Model. Also added two migrations accordingly.

--- VideGo/Models/Customer.cs
+++ VideGo/Models/Customer.cs
@@ -10,5 +10,8 @@ namespace VideGo.Models
     {   
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
