58cfd66f59587d478dd80781484482437293ca41
bdunams <dabadestboy1@gmail.com>
2017-11-25 15:28:36 -0500

    Added CamelCasePropertyNames and json return format for API data format.

--- VideGo/App_Start/WebApiConfig.cs
+++ VideGo/App_Start/WebApiConfig.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
 
 namespace VideGo
 {
@@ -9,6 +13,16 @@ namespace VideGo
     {
         public static void Register(HttpConfiguration config)
         {
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+
+            config.Formatters.JsonFormatter.MediaTypeMappings
+                .Add(new QueryStringMapping("json", "true", "application/json"));
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
