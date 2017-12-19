fa91cbe17934b54057a60736d93a9acf857f7726
bdunams <dabadestboy1@gmail.com>
2017-12-18 20:24:42 -0500

    Modified CreateNewRental action

--- VideGo/Controllers/Api/NewRentalController.cs
+++ VideGo/Controllers/Api/NewRentalController.cs
@@ -26,7 +26,28 @@ namespace VideGo.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRental(NewRentalDTO newRental)
         {
-            return BadRequest();
+            if(!ModelState.IsValid)
+                return BadRequest();
+
+            var customer = _context.Customers.Single(
+                c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies.Where(
+                m => newRental.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            return Ok();
         }
     }
 }
