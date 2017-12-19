ac49a87792afb150a4f590851e05541f129221e1
bdunams <dabadestboy1@gmail.com>
2017-12-18 19:43:24 -0500

    Added API controller to handle customer rental requests.

---
+++ VideGo/Controllers/Api/NewRentalController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VideGo.DTOs;
+using VideGo.Models;
+
+namespace VideGo.Controllers.Api
+{
+    public class NewRentalController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRental(NewRentalDTO newRental)
+        {
+            return BadRequest();
+        }
+    }
+}
---
+++ VideGo/DTOs/NewRentalDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideGo.DTOs
+{
+    public class NewRentalDTO
+    {
+        public int CustomerId { get; set; }
+        public List<int> MovieIds { get; set; }
+    }
+}
